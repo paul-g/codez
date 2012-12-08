ba39806ee424c252fbb4e0c6713bdd9544250e42
Paul Grigoras <paul.grigoras90@gmail.com>
2012-12-08 02:48:40 +0000

    Remove the demo polls app. Close #2.

--- codez/settings.py
+++ codez/settings.py
@@ -129,7 +129,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'registration',
-    'polls',
     'judge',
 )
 
--- codez/urls.py
+++ codez/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^polls/', include('polls.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^accounts/', include('registration.backends.default.urls')),
 	url(r'', include('judge.urls')),
